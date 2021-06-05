565068df04987a4548a81e4800e0881a24ea1ca4
Luis Pimentel <lpimentel3@gatech.edu>
2021-06-05 17:43:58 -0600

    adds ability to build mlp

--- hw1/cs285/infrastructure/pytorch_util.py
+++ hw1/cs285/infrastructure/pytorch_util.py
@@ -45,9 +45,20 @@ def build_mlp(
     if isinstance(output_activation, str):
         output_activation = _str_to_activation[output_activation]
 
+
+
     # TODO: return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
-    raise NotImplementedError
+
+    layer_sizes = [input_size] + [size for _ in range(n_layers)] + [output_size]
+    layers = []
+
+    for i in range(len(layer_sizes)-1):
+        act = activation if i < len(layer_sizes)-2 else output_activation
+        layers += [nn.Linear(layer_sizes[i], layer_sizes[i+1]) , act]
+
+    return nn.Sequential(*layers)
+
 
 
 device = None
